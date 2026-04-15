5153a28fa87a2b2a1807682d6328d844bf737d5a
user <araliyakrishantha91@gmail.com>
2026-04-15 15:19:16 +0530

    commit 2

--- string.py
+++
@@ -1,8 +0,0 @@
-print('python is fun')
-print("pythons string's are easy to use")
-print('we can include also "quotes" in strings')
-print("hello " + "world")
-greeting = "welcome"
-name = input("please enter your name")
-print(greeting + name)
-print(greeting + " " + name)
