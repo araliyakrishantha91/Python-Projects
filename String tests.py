b203e366d19b3127348d6286147ff2fdfb702f3a
user <araliyakrishantha91@gmail.com>
2026-04-18 18:34:19 +0530

    commit 3

---
+++ String tests.py
@@ -0,0 +1,11 @@
+print("Hello world")
+print('Learning python is fun')
+print("Nimal's car is red color")
+print('This might highlights by','the "quotation" marks')
+print('This might highlights by the "quotation" marks')
+print("Hello" , "World")
+print("Hello " + " World")
+greeting = "Hello"
+name = input("Enter your name")
+
+print(greeting+ ' ' +name)
---
+++ backslash.py
@@ -0,0 +1,21 @@
+SplitString = "this\nsentence\nhas\nsplit\nseveral\ntimes"
+print(SplitString)
+TabTest = "1\t2\t3\t4\t5"
+print(TabTest)
+print("This is 'e 's \"test\" and 'you must \"see ")
+print('This is \'e \'s "test" and \'you must "see ')
+print("""This is 'e 's "test" \
+and 'you must "see""")
+print("""Hi 
+all
+how 
+are 
+you?""")
+#we can avoid by backslash
+print("""Hi \
+all \
+how \
+are \
+you?""")
+print("C\\Program\\Users\\Python\\Sysfiles")
+print(r"C\Program\Users\Python\Sysfiles")
